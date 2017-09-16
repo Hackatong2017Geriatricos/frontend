a22d7abc1e38032114646fcd32f68f03ac724a74
Broda Noel <brodanoel@gmail.com>
2017-09-16 16:53:29 -0300

    Agregar estilos para el evento hover

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 var $ = document.querySelector.bind(document);
 
+/**
+ * @desc Esta funcion va a ser usada para cambiar la URL cuando el usuario hace click en la barra de navegacion
+ * @param  {String} pagina - El nombre que transformaremos a URL
+ */
+function cambiarURL(pagina) {
+	// Eliminamos la clase "activo" de todos los <li>
+	document.querySelectorAll('.navbar li').forEach(function(item) {
+		item.classList.remove('activo');
+	});
+
+	// Agregamos la clase "activo" al <li> que se le hizo click
+	$('.navbar li.' + pagina).classList.add('activo');
+
+	// Cambiamos el contenido de la pagina
+	$('iframe').src = './' + pagina + '.html';
+}
+
 window.onload = function() {
 	// Cuando se refresca la pagina, volver a cargar la pagina en la que estaba el usuario
 	var pagina = window.location.hash.replace('#', '');
 
 	if (pagina.length > 0) {
-		$('iframe').src = './' + pagina + '.html';
+		cambiarURL(pagina);
 	}
 };
