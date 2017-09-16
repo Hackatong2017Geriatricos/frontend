f88509cfa14c5d835988c5f8d7110d6236e590bd
Broda Noel <brodanoel@gmail.com>
2017-09-16 15:24:13 -0300

    Cargar pagina correctamente luego de refrescar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 var $ = document.querySelector.bind(document);
 
 window.onload = function() {
-	//
+	// Cuando se refresca la pagina, volver a cargar la pagina en la que estaba el usuario
+	var pagina = window.location.hash.replace('#', '');
+
+	if (pagina.length > 0) {
+		$('iframe').src = './' + pagina + '.html';
+	}
 };
