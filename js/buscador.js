ce170784252f2ac69dc4f5b527d019a2578a9b46
Broda Noel <brodanoel@gmail.com>
2017-09-16 14:28:14 -0300

    Mostrar informacion detallada del Geriatrico

--- js/buscador.js
+++ js/buscador.js
@@ -4,7 +4,24 @@ var geriatricos = [
 	{ nombre: 'Bla bla', coor: { lat: -31.4164778, lng: -64.1919612 }, direccion: 'Indep.' },
 	{ nombre: 'Otro mas', coor: { lat: -31.41, lng: -64.19 }, direccion: 'Estrada' }
 ];
-var template = '<div id="icono">Nombre: {nombre}<br />Direccion: {direccion}</div>';
+var templateMapa = '<div id="icono">Nombre: {nombre}<br />Direccion: {direccion}</div>';
+var infoWindowVisible = null;
+
+/**
+ * @desc Esta funcion va a ser llamada cada vez que se haga click en un punto en el mapa (Marker).
+ * Aca hay que hacer la logica relacionada a mostrar los datos del geriatrico, en el panel izquierdo,
+ * debajo de los filtros
+ * @param  {Object} geriatrico - El objeto que contiene toda la informacion del geriatrico
+ */
+function mostrarInformacion(geriatrico) {
+	if ($('.info')) {
+		$('.info').remove();
+	}
+
+	$('.geriatrico').style = '';
+	$('.geriatrico .nombre').innerHTML = geriatrico.nombre;
+	$('.geriatrico .direccion').innerHTML = geriatrico.direccion;
+}
 
 /**
  * @desc Esta function es ejecutada por la api de Google Maps cuando el mapa termina de cargarse.
@@ -24,7 +41,7 @@ function iniciarMapa() {
 			title: geriatrico.nombre
 		});
 
-		var templateFinal = template
+		var templateFinal = templateMapa
 			.replace('{nombre}', geriatrico.nombre)
 			.replace('{direccion}', geriatrico.direccion);
 
@@ -33,7 +50,14 @@ function iniciarMapa() {
 		});
 
 		marker.addListener('click', function() {
+			if (infoWindowVisible) {
+				infoWindowVisible.close();
+			}
+
 			infowindow.open(map, marker);
+			infoWindowVisible = infowindow;
+
+			mostrarInformacion(geriatrico);
 		});
 	});
 }
