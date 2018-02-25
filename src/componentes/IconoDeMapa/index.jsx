5910d5c28d2f48b0423efbd586a95802c883df1f
Broda Noel <brodanoel@gmail.com>
2018-02-25 14:23:42 -0300

    Agregar Icono de Mapa para usar en Google Maps

---
+++ src/componentes/IconoDeMapa/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import icon from './icono.png';
+import './estilos.css';
+
+const IconoDeMapa = () => <img className="IconoDeMapa" src={icon} alt="icon" />;
+
+export default IconoDeMapa;
