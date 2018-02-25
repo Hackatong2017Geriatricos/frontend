1b613271d32bcc699ad4c7ae3f9edd5c25e584ec
Broda Noel <brodanoel@gmail.com>
2018-02-25 14:07:15 -0300

    Empezar a crear el Buscador

--- src/paginas/Buscador/index.jsx
+++ src/paginas/Buscador/index.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Mapa from 'contenedores/Mapa';
+import './estilos.css';
 
 class Buscador extends Component {
   render() {
     return (
       <div className="Buscador">
-        Buscador!
+        <div className="izquierda">
+          <input type="text" placeholder="Buscar por nombre" />
+        </div>
+
+        <div className="derecha">
+          <Mapa></Mapa>
+        </div>
       </div>
     );
   }
