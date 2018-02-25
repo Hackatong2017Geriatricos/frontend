0f705cdf7b1c086ce3476894a9e813fccc8c14fc
Broda Noel <brodanoel@gmail.com>
2018-02-25 13:31:24 -0300

    Mejorar Pie

--- src/componentes/Pie/index.js
+++ src/componentes/Pie/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import './estilos.css';
+import fundeps from 'estilos/imagenes/fundeps.png';
+import municipalidad from 'estilos/imagenes/municipalidad.png';
+import nacion from 'estilos/imagenes/nacion.png';
+import provincia from 'estilos/imagenes/provincia.png';
+import tic from 'estilos/imagenes/tic.png';
 
 const Pie = () =>
   <div className="Pie">
-    Pie
+    <div className="columna">
+      <img src={fundeps} alt="Logo de Fundeps" />
+    </div>
+
+    <div className="columna">
+      <img src={municipalidad} alt="Logo de la Municipalidad" />
+    </div>
+
+    <div className="columna">
+      <img src={nacion} alt="Logo de la Nacion" className="nacion" />
+    </div>
+
+    <div className="columna">
+      <img src={provincia} alt="Logo de la Provincia" />
+    </div>
+
+    <div className="columna">
+      <img src={tic} alt="Logo de TICs" />
+    </div>
   </div>;
 
 export default Pie;
