6050d5a163e04e2b232c57ed63f704f057fed5f7
Broda Noel <brodanoel@gmail.com>
2018-02-25 14:24:32 -0300

    Agregar Mapa

--- src/contenedores/Mapa/index.jsx
+++ src/contenedores/Mapa/index.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import IconoDeMapa from 'componentes/IconoDeMapa';
+import config from 'config';
+import './estilos.css';
 
 class Mapa extends Component {
+  state = {
+    center: { lat: 10, lng: -35 },
+    zoom: 0,
+    points: []
+  }
+
   render() {
     return (
       <div className="Mapa">
-        Mapa aqui
+        <GoogleMapReact
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
+          bootstrapURLKeys={{key: config.googleMaps.apiKey}}>
+          {
+            this.state.points.map((point, index) => <IconoDeMapa key={index} {...point} />)
+          }
+        </GoogleMapReact>
       </div>
     );
   }
