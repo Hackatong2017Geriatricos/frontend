5bfd83402eee83d8751ccf07583bb9d0f8c5fe4b
Broda Noel <brodanoel@gmail.com>
2018-02-25 16:12:45 -0300

    Arreglar estilos de Mapa

--- src/contenedores/Mapa/index.jsx
+++ src/contenedores/Mapa/index.jsx
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import GoogleMap from 'google-map-react';
 import IconoDeMapa from 'componentes/IconoDeMapa';
 import config from 'config';
 import './estilos.css';
 
 class Mapa extends Component {
   state = {
-    center: { lat: 10, lng: -35 },
-    zoom: 0,
-    points: []
+    center: { lat: -31.41, lng: -64.18 },
+    zoom: 12,
+    /**
+     * @example
+     * { lat: 59.955413, lng: 30.337844 }
+     * Mas informacion: https://github.com/istarkov/google-map-react
+     * @type {Array}
+     */
+    puntosEnMapa: [
+      { lat: -31.41, lng: -64.18 }
+    ]
   }
 
   render() {
     return (
       <div className="Mapa">
-        <GoogleMapReact
+        <GoogleMap
           defaultCenter={this.state.center}
           defaultZoom={this.state.zoom}
-          bootstrapURLKeys={{key: config.googleMaps.apiKey}}>
+          bootstrapURLKeys={{
+            key: config.googleMaps.apiKey,
+            // TODO: Eliminar esto en un futuro y probar hacer zoom en el mapa.
+            // Actualmente, tengo que forzar el uso de `3.30` porque al hacer
+            // zoom en el mapa, los Markers (puntos del mapa) se re-dibujan
+            // mediante una animacion desde las esquinas del mapa.
+            // Parece ser un bug de la nueva version de Google Maps.
+            // Mas info: https://github.com/istarkov/google-map-react/issues/510
+            v: '3.30'
+          }}>
           {
-            this.state.points.map((point, index) => <IconoDeMapa key={index} {...point} />)
+            this.state.puntosEnMapa.map((punto, index) =>
+              <IconoDeMapa
+                key={index} {...punto} />)
           }
-        </GoogleMapReact>
+        </GoogleMap>
       </div>
     );
   }
