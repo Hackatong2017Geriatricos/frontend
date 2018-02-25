7e69a24817efe086e7fc25562a02c680694097f9
Broda Noel <brodanoel@gmail.com>
2018-02-25 14:07:06 -0300

    Crear componente base Mapaa

---
+++ src/contenedores/Mapa/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Mapa extends Component {
+  render() {
+    return (
+      <div className="Mapa">
+        Mapa aqui
+      </div>
+    );
+  }
+}
+
+export default Mapa;
